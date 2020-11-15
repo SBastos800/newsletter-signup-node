65b2f1775403bda10d2b50ab168104ae052b1501
SBastos800 <soniavsbastos@gmail.com>
2020-11-15 21:36:21 +0000

    added app.post, added action and method to the form in the html file + names to each input

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.get('/', (req, res) => {
 res.sendFile(__dirname + '/signup.html');
 });
 
-
+app.post('/', (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.inputEmail;
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000.');
